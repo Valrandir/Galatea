efec768bf166188d85f597a3e76a9b0c652c5ae8
Valrandir <valrandir@voxcel.net>
2012-12-04 15:06:57 -0500

    Types

--- Source/CoreLib/Types.hpp
+++ Source/CoreLib/Types.hpp
@@ -6,12 +6,21 @@
 
 namespace Core
 {
-	typedef signed short Int16;
-	typedef signed int Int32;
-	typedef signed long long Int64;
-	typedef unsigned short UInt16;
-	typedef unsigned int UInt32;
-	typedef unsigned long long UInt64;
+	#ifdef CoreTargetWin32
+		typedef signed __int16 Int16;
+		typedef signed __int32 Int32;
+		typedef signed __int64 Int64;
+		typedef unsigned __int16 UInt16;
+		typedef unsigned __int32 UInt32;
+		typedef unsigned __int64 UInt64;
+	#else
+		typedef signed short Int16;
+		typedef signed int Int32;
+		typedef signed long long Int64;
+		typedef unsigned short UInt16;
+		typedef unsigned int UInt32;
+		typedef unsigned long long UInt64;
+	#endif
 
 	#if defined(CoreTargetWin32) && defined(UNICODE)
 		#define Text(quote)L##quote
