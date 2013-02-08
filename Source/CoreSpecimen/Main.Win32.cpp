a1ab3439df38d31da125e8cd33ea468803f881ba
Valrandir <valrandir@voxcel.net>
2013-02-08 01:39:33 -0500

    Vector - Added CtorModeEnum Once
    - Changed RawCopyEnum to CtorModeEnum
    - Revised support for Pod, Once, Always
    - Adapted Thread Tests to use ASSERT

--- Source/CoreSpecimen/Main.Win32.cpp
+++ Source/CoreSpecimen/Main.Win32.cpp
@@ -1,7 +1,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-extern void CoreSpecimen();
+extern bool CoreSpecimen();
 
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR,  _In_ int)
 {
