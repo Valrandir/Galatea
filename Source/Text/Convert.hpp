34dd8106cfb03de3365e76c8444c714cececa806
valrandir <valrandir@voxcel.net>
2016-03-02 14:16:12 -0500

    Text Convert

--- Source/Text/Convert.hpp
+++ Source/Text/Convert.hpp
@@ -10,5 +10,11 @@ namespace Galatea
 
 		//Must delete the returned pointer when done
 		const char* WideToAnsii(const char* source);
+
+		//Must delete the returned pointer when done
+		const wchar_t* AnsiiToWide(const char* source);
+
+		//Must delete the returned pointer when done
+		const wchar_t* AnsiiToWide(const wchar_t* source);
 	}
 }
--- Source/Text/Win32/Convert.Win32.cpp
+++ Source/Text/Win32/Convert.Win32.cpp
@@ -17,8 +17,27 @@ namespace Galatea
 		//Must delete the returned pointer when done
 		const char* WideToAnsii(const char* source)
 		{
-			const size_t byte_size = strlen(source);
+			const size_t byte_size = strlen(source) * sizeof(char);
 			char* buffer = new char[byte_size];
+			for(size_t i = 0; i < byte_size; ++i)
+				buffer[i] = source[i];
+			return buffer;
+		}
+
+		//Must delete the returned pointer when done
+		const wchar_t* AnsiiToWide(const char* source)
+		{
+			int byte_size = MultiByteToWideChar(CP_UTF8, 0, source, -1, nullptr, 0);
+			wchar_t* buffer = new wchar_t[byte_size];
+			MultiByteToWideChar(CP_UTF8, 0, source, -1, buffer, byte_size);
+			return buffer;
+		}
+
+		//Must delete the returned pointer when done
+		const wchar_t* AnsiiToWide(const wchar_t* source)
+		{
+			const size_t byte_size = wcslen(source) * sizeof(wchar_t);
+			wchar_t* buffer = new wchar_t[byte_size];
 			for(size_t i = 0; i < byte_size; ++i)
 				buffer[i] = source[i];
 			return buffer;
